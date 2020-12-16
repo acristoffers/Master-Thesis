65576d42fd93a5983ddb36eff3ef64e7a3c92f27
Álan Crístoffer <acristoffers@gmail.com>
2020-12-16 10:45:34 -0300

    Changes all captions.

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,4 +1,4 @@
-((LaTeX-mode . ((TeX-master . "document.tex")
-                (reftex-default-bibliography . "bibliothek.bib")))
- (latex-mode . ((TeX-master . "document.tex")
-                (reftex-default-bibliography . "bibliothek.bib"))))
+((LaTeX-mode . ((eval . (setq-local TeX-master (concat (file-name-directory (or load-file-name buffer-file-name)) "document.tex")))
+                (eval . (setq-local reftex-default-bibliography (concat (file-name-directory (or load-file-name buffer-file-name)) "bibliothek.bib")))))
+ (latex-mode . ((eval . (setq-local TeX-master (concat (file-name-directory (or load-file-name buffer-file-name)) "document.tex")))
+                (eval . (setq-local reftex-default-bibliography (concat (file-name-directory (or load-file-name buffer-file-name)) "bibliothek.bib"))))))
